ec551f08f2d19c9457ed211555c35c0623092c49
Uditha <uditha_a@live.com>
2021-07-18 21:24:18 -0400

    creating properties

---
+++ WebAppAst2/Data/Todo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppAst2.Data
+{
+    public class Todo
+    {
+        [Key]
+        public int Id { get; set; }
+        public Boolean Done { get; set;}
+        [StringLength(255)]
+        public String Name { get; set; }
+
+        public DateTime EntryDate { get; set; } = DateTime.Now;
+
+    }
+}
--- WebAppAst2/Pages/Index.cshtml.cs
+++ WebAppAst2/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAppAst2.Data;
 
 namespace WebAppAst2.Pages
 {
